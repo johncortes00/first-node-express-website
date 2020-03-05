22620fde50642b09cd7d87aca0432b0abcf3cdd7
John Cortes <john.cortes00@usc.edu.co>
2020-03-04 22:03:17 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+console.log("Server listening");
+
+const express = require('express')
+const app = express();
+const path = require('path');
+
+// settings
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs')
+
+// middlewares
+
+// routes
+app.use(require('./routes/index'));
+/*app.get('/', (req, res) => {
+    res.render('index', { title: 'First Website' });
+});*/
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// listening the server
+app.listen(app.get('port'), () => {
+    console.log("Server on port: " + app.get('port'));
+});

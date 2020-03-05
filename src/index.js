c11a9a5bcfe26f3a525da44925c551a58165bbf6
John Cortes <john.cortes00@usc.edu.co>
2020-03-04 22:50:53 -0500

    changing port

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 console.log("Server listening");
 
-const express = require('express')
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
 // settings
-app.set('port', 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs')
+app.set("port", 3000);
+app.set("views", path.join(__dirname, "views"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "ejs");
 
 // middlewares
 
 // routes
-app.use(require('./routes/index'));
+app.use(require("./routes/index"));
 /*app.get('/', (req, res) => {
     res.render('index', { title: 'First Website' });
 });*/
 
 // static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // listening the server
-app.listen(app.get('port'), () => {
-    console.log("Server on port: " + app.get('port'));
+app.listen(process.env.PORT || app.get("port"), () => {
+  console.log("Server on port: " + app.get("port"));
 });
